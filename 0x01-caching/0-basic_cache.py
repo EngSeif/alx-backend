770e4e16b1f2a1ccbe92d17d30750b2b35d0fe79
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-30 14:53:30 +0300

    Cache | unlimited Cache | fix style

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,7 +4,12 @@
 
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class BasicCache(BaseCaching):
+    """
+    class BasicCache that inherits from
+    BaseCaching and is a caching system
+    """
 
     def put(self, key, item):
         """
