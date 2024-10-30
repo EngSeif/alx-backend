643250dc83c42241bc253ed0dd5f493c9bd30202
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-30 14:52:13 +0300

    Cache | unlimited Cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if (key is not None or item is not None):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Must return the value
+        in self.cache_data linked to key.
+        """
+        if key is not None:
+            return self.cache_data.get(key, None)
